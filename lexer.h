81558bce3acb0e7673574501531c815e6fbe338c
LanKuDot <lankudot@github.com>
2014-05-29 12:50:30 +0800

    The enumeration of the category

--- lexer.h
+++ lexer.h
@@ -1,6 +1,19 @@
 #ifndef LEXER_H
 #define LEXER_H
 
+/* The enumeration of the category of tokens */
+enum Category {
+	KEYWORD = 0,
+	OPERATOR,
+	SPECIAL_SYMBOL,
+	IDENTIFIER,
+	CHAR,
+	NUMBER,
+	COMMENT,
+	ERROR,
+	CATEGORY_COUNT
+};
+
 int lexial_analyzer( const char *source_file );
 
 #endif	/* LEXER_H */
