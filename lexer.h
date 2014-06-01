dded13e62f74ff68500a8b85ff9f7d879baaa71c
brandboat <brandboat@gmail.com>
2014-06-01 16:02:51 +0800

    do nothing

--- lexer.h
+++ lexer.h
@@ -1,6 +1,12 @@
 #ifndef LEXER_H
 #define LEXER_H
 
+#include <string>
+#include <vector>
+
+#define OUTPUT_FILE_NAME	"token.txt"
+
+using namespace std; 
 /* The enumeration of the category of tokens */
 enum Category {
 	KEYWORD = 0,
@@ -14,6 +20,16 @@ enum Category {
 	CATEGORY_COUNT
 };
 
-int lexial_analyzer( const char *source_file );
+/* The information of a token */
+typedef struct {
+	string token;
+	string tranToken;
+	enum Category category;
+} Token_info;
+
+/* The string of Category */
+extern char category_str[CATEGORY_COUNT][16];
+
+int lexial_analyzer( const char *source_file, vector<Token_info> *token_list );
 
 #endif	/* LEXER_H */
