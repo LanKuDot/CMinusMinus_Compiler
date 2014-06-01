2c577525eabf06c1b388495a124a6890e754dec5
LanKuDot <lankudot@github.com>
2014-06-01 16:43:51 +0800

    Macros of the related files

--- CMinusMinus.cpp
+++ CMinusMinus.cpp
@@ -2,15 +2,22 @@
 #include "lexer.h"
 #include "Syntax_Analyzer.h"
 
+/* The marcos of related files */
+#define SOURCE_CODE_FILE	"main.c"
+#define GRAMMAR_FILE		"grammar.txt"
+#define PARSE_TREE_FILE		"Parse_Tree.txt"
+#define SET_FILE			"Set.txt"
+#define LLTABLE_FILE		"Table.txt"
+
 using namespace std;
 
 int main()
 {
 	/* lexial_analyzer */
-	lexial_analyzer("main.c", & tokenList);
+	lexial_analyzer( SOURCE_CODE_FILE, & tokenList);
 
 	/* syntax analyzer */
-	readFile("grammar.txt");
+	readFile( GRAMMAR_FILE );
 	// create terminal and nonterminal set
 	createSet();
 	createNullTable();
@@ -18,11 +25,11 @@ int main()
 	createFollowTable();
 	createLLTable();
 	// create parse tree
-	parser(tokenList, "Parse_Tree.txt");
+	parser(tokenList, PARSE_TREE_FILE );
 
 	printTermNonterminal();
-	printSet("Set.txt");
-	printLLTable("Table.txt");
+	printSet(SET_FILE);
+	printLLTable(LLTABLE_FILE);
 
 	return 0;	
 }
