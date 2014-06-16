132787d77709af02dbff78f0fc998eabbd7dc365
LanKuDot <lankudot@github.com>
2014-06-16 15:05:53 +0800

    Seperate the macro of filename

--- CMinusMinus.cpp
+++ CMinusMinus.cpp
@@ -1,13 +1,7 @@
 #include <iostream>
 #include "lexer.h"
 #include "Syntax_Analyzer.h"
-
-/* The marcos of related files */
-#define SOURCE_CODE_FILE	"main.c"
-#define GRAMMAR_FILE		"grammar.txt"
-#define PARSE_TREE_FILE		"Parse_Tree.txt"
-#define SET_FILE			"Set.txt"
-#define LLTABLE_FILE		"Table.txt"
+#include "config.h"
 
 using namespace std;
 
---
+++ config.h
@@ -0,0 +1,11 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/* The marcos of related files */
+#define SOURCE_CODE_FILE	"main.c"
+#define GRAMMAR_FILE		"grammar.txt"
+#define PARSE_TREE_FILE		"Parse_Tree.txt"
+#define SET_FILE			"Set.txt"
+#define LLTABLE_FILE		"Table.txt"
+
+#endif /* CONFIG_H */
