74cc9b58928dc3764e964ca3a739fdc4cd2b4979
LanKuDot <lankudot@github.com>
2014-06-18 04:05:13 +0800

    Remove the file macros in CMinusMinus.cpp

--- CMinusMinus.cpp
+++ CMinusMinus.cpp
@@ -6,14 +6,6 @@
 #include "MachineCodeGenerator.h"
 #include "config.h"
 
-/* The marcos of related files */
-#define SOURCE_CODE_FILE	"main.c"
-#define GRAMMAR_FILE		"grammar.txt"
-#define PARSE_TREE_FILE		"Parse_Tree.txt"
-#define SET_FILE			"Set.txt"
-#define LLTABLE_FILE		"Table.txt"
-#define SYMBOL_TABLE_FILE	"Symbol_Table.txt"
-
 using namespace std;
 
 vector<Symbol_Detail> Symbol_Table_Element;
