0a250824acb27d00c54354148dba57f049d39452
brandboat <brandboat@gmail.com>
2014-06-17 19:50:04 +0800

    update Semantic_Analyzer.h

--- Semantic_Analyzer.h
+++ Semantic_Analyzer.h
@@ -11,12 +11,24 @@
 #include "lexer.h"
 using namespace std;
 
+// the Symbol_Detail records the detail of the symbol and will be
+// stored into Symbol_Table.
+typedef struct {
+	string symbol;
+	string token;
+	bool initialized;
+	bool declared; 
+	int scope;
+	string type;
+} Symbol_Detail;
+
+extern vector<Symbol_Detail> Symbol_Table_Element;
+
 bool isDeclared(int);
 bool isInitialized(int);
 
 string lookUp (int, int, bool, bool);
 void insert(int, string, int, bool, bool);
-
 void createSymbolTable(const char * fileName);
 
 #endif /* SEMANTIC_ANZR */
